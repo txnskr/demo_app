ce0a310f60703b3de9ade8626be676c3107eff38
Clay Kramer <ckramer@gmail.com>
2012-05-21 17:21:56 -0700

    Added config.assets.precompile += %w( *.js *.css ) to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ DemoApp::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.precompile += %w( *.js *.css)
 
   # Generate digests for assets URLs
   config.assets.digest = true
